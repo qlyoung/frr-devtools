6536f50646f52314248d40921da481cf967bcd00
Quentin Young <qlyoung@cumulusnetworks.com>
2020-04-14 17:16:11 -0400

    scripts: update install-frr.sh
    Buncha tweaks / fixes
    - Change default compiler to gcc
    - Remove bear by default, most ppl don't have it
    - Use existing configure.ac options for coverage
    - Add -g for enabling gcov
    - Fix -v for verbose make
    - Add examples, copyright
    - change -f to -F
    - fix help text
    - fix a bunch of shellcheck issues
    - enable everything for ubsan
    - always use -g3 -O3 for -F and -Z builds
    - always use -Wno-all for -F and -Z builds
    
    Signed-off-by: Quentin Young <qlyoung@cumulusnetworks.com>

--- scripts/install-frr.sh
+++ scripts/install-frr.sh
@@ -1,7 +1,29 @@
 #!/bin/bash
+# Copyright (c) Quentin Young 2017-2020
+#
+# High level configuration & build script for FRR.
+# Wraps common options, developer tools, config switches etc.
+#
+# Examples:
+#
+# Typical build:
+#    ./install-frr.sh -boid frr
+#
+# ASAN build:
+#    ./install-frr.sh -boiad frr
+#
+# ASAN, UBSAN and libFuzzer build:
+#    ./install-frr.sh -boiauZd frr
+#
+# gcov build:
+#    ./install-frr.sh -boigd frr
+#
+# ASAN, UBSAN, TSAN, clang coverage, statically linked AFL build:
+#    ./install-frr.sh -boiautrsd frr
 
-# A POSIX variable
-OPTIND=1         # Reset in case getopts has been used previously in the shell.
+
+# Reset in case getopts has been used previously in the shell.
+OPTIND=1
 
 # Initialize our own variables:
 configure=0
@@ -9,7 +31,7 @@ bootstrap=0
 verbose=0
 clean=0
 scanbuild=""
-bear="bear"
+bear=""
 install=0
 jobz=4
 dir="frr"
@@ -19,16 +41,16 @@ llvmconfig=$(which llvm-config)
 
 ulimit -v unlimited
 
-mycc="clang"
+mycc="gcc"
 mycflags="-g3 -O0"
 
-while getopts "d:hobyvcij:taemfx:Zrsu" opt; do
+while getopts "hobyvcij:d:eatmuFZrgsx:" opt; do
 	case "$opt" in
 		h)
 			echo "-h -- display help"
 			echo "-o -- ./configure"
 			echo "-b -- ./bootstrap.sh"
-			echo "-s -- use scan-build"
+			echo "-y -- use scan-build"
 			echo "-v -- verbose"
 			echo "-c -- make clean"
 			echo "-i -- clean install"
@@ -39,9 +61,10 @@ while getopts "d:hobyvcij:taemfx:Zrsu" opt; do
 			echo "-t -- enable thread sanitizer"
 			echo "-m -- enable memory sanitizer"
 			echo "-u -- enable undefined behavior sanitizer"
-			echo "-f -- build as fuzzing targets for AFL"
+			echo "-F -- build as fuzzing targets for AFL"
 			echo "-Z -- build as fuzzing targets for libFuzzer"
 			echo "-r -- build with LLVM coverage instrumentation"
+			echo "-g -- build with gcov coverage instrumentation"
 			echo "-s -- use static linking"
 			echo "-x -- add to CFLAGS"
 			exit
@@ -70,6 +93,9 @@ while getopts "d:hobyvcij:taemfx:Zrsu" opt; do
 		d)
 			dir=$OPTARG
 			;;
+		e)
+			bear="bear"
+			;;
 		a)
 			extra_configure_switches+=" --enable-address-sanitizer"
 			;;
@@ -80,25 +106,27 @@ while getopts "d:hobyvcij:taemfx:Zrsu" opt; do
 			extra_configure_switches+=" --enable-memory-sanitizer"
 			;;
 		u)
-			mycflags+=" -fno-sanitize-recover=all"
+			mycflags+=" -fno-sanitize-recover=all -fsanitize=unsigned-integer-overflow,implicit-conversion,nullability-arg,nullability-assign,nullability-return"
 			extra_configure_switches+=" --enable-undefined-sanitizer"
 			;;
-		e)
-			bear="bear"
-			;;
-		f)
+		F)
 			mycc="afl-clang-fast"
-			mycflags+=" -g3 -O0 -funroll-loops -fsanitize-trap=all"
+			mycflags+=" -Wno-all -g3 -O3 -funroll-loops -fsanitize-trap=all"
 			aflharden=1
 			;;
 		Z)
 			mycc="clang"
 			extra_configure_switches+=" --enable-libfuzzer"
-			mycflags+=" -g3 -O0 -funroll-loops"
+			mycflags+=" -Wno-all -g3 -O3 -funroll-loops"
 			;;
 		r)
-			echo "Building with code coverage enabled"
-			mycflags++=" -g -fprofile-instr-generate -fcoverage-mapping"
+			echo "Building with ClangCoverage enabled"
+			mycflags+=" -g3"
+			extra_configure_switches+=" --enable-clang-coverage"
+			;;
+		g)
+			echo "Building with gcov enabled"
+			extra_configure_switches+=" --enable-gcov"
 			;;
 		s)
 			extra_configure_switches+=" --enable-shared --enable-static --enable-static-bin"
@@ -113,8 +141,8 @@ echo "CFLAGS: $mycflags"
 echo "CC: $mycc"
 echo "AFL_HARDEN: $aflharden"
 echo "LLVM_CONFIG: $llvmconfig"
-echo "extra_configure_switches: $extra_configure_switches"
-cd $dir
+echo "Extra configure options: $extra_configure_switches"
+cd "$dir" || (printf "No such directory '%s'" "$dir"; exit 1)
 
 if [ $bootstrap -gt 0 ]; then
 	./bootstrap.sh
@@ -128,14 +156,14 @@ if [ $configure -gt 0 ]; then
 	$scanbuild ./configure \
 		--build=x86_64-linux-gnu \
 		--prefix=/usr \
-		--includedir=\${prefix}/include \
-		--mandir=\${prefix}/share/man \
-		--infodir=\${prefix}/share/info \
+		--includedir="\${prefix}/include" \
+		--mandir="\${prefix}/share/man" \
+		--infodir="\${prefix}/share/info" \
+		--libexecdir="\${prefix}/lib/frr" \
+		--enable-exampledir="\${prefix}/share/doc/frr/examples/" \
+		--sbindir="\${prefix}/lib/frr" \
 		--sysconfdir=/etc/ \
-		--libexecdir=\${prefix}/lib/frr \
-		--enable-exampledir=/usr/share/doc/frr/examples/ \
 		--localstatedir=/var/run/frr \
-		--sbindir=/usr/lib/frr \
 		--sysconfdir=/etc/frr \
 		--enable-ospfclient=yes \
 		--enable-ospfapi=yes \
@@ -161,6 +189,7 @@ if [ $configure -gt 0 ]; then
 		--enable-doc=yes \
 		--enable-doc-html=yes \
 		--enable-static=yes \
+		--enable-grpc=yes \
 		$extra_configure_switches
 		#--enable-snmp=agentx \
 fi
@@ -170,13 +199,13 @@ if [ $clean -gt 0 ]; then
 fi
 
 if [ $install -gt 0 ]; then
-	$scanbuild $bear make -j $jobz
+	$scanbuild $bear make V=$verbose -j "$jobz"
 	# install
 	systemctl stop frr
 	rm -rf /var/log/frr/*
 	rm -rf /var/support/*
 	rm /usr/lib/frr/*
-	make install
+	make V=$verbose install
 	systemctl reset-failed frr
 fi
 exit 0
